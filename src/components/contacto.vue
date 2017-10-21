44ac7b5ce8eda0925521a4cf659ddc5106ff292f
david <david.parra@udea.edu.co>
2017-10-21 12:27:19 -0500

    commit final

--- src/components/contacto.vue
+++ src/components/contacto.vue
@@ -1,8 +1,22 @@
 <template>
-<h2>Contacto</h2>
+<div  id='contact1'>
+  <h2>Contacto</h2>
+  <ul>
+    <li v-for= "contacto in contactos">{{contacto}}</li>
+    </ul>
+
+</div>
 </template>
 
 <script>
+export default{
+data(){
+  return {contactos:[
+    '',
+    'Elma Marcela Rico'
+  ]}
+}}
+
 </script>
 
 <style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,11 +1,29 @@
 <template>
-<div>
-<h2>Home</h2>
-</div>
+  <div>
+    <h2>Home</h2>
+    <input type="text" placeholder="Introducir tarea" v-model="TextoNuevo">
+    <input type="button" value="Enviar texto" v-on:click="cambiarTexto">
+    <h2>{{tarea}}</h2>
+
+  </div>
 </template>
 
 <script>
+export default{
+data(){
+    return{
+      TextoNuevo:null,
+      tarea:null
+    }
+  },
+  methods:{
+    cambiarTexto(){
+      this.tarea=this.TextoNuevo;
+    },
+},
+}
 </script>
+
 
 <style>
 </style>
